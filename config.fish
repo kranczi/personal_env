1b455b653ae5cac5c5e7f8f659abf5413cef1b27
Marcin Adamski <adamski.marcin@gmail.com>
2018-07-27 23:59:50 +0200

    add config.fish

---
+++ config.fish
@@ -0,0 +1,6 @@
+powerline-daemon -q
+set POWERLINE_SHELL_CONTINUATION 1
+set POWERLINE_SHELL_SELECT 1
+set fish_function_path $fish_function_path "/usr/local/lib/python3.6/dist-packages/powerline/bindings/fish
+"
+powerline-setup
