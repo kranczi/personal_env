69b51b305a0a753522f67ba8978f432d45669036
Marcin Adamski <adamski.marcin@gmail.com>
2019-02-13 23:40:09 +0100

    add path and pythonpath to config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 powerline-daemon -q
 set POWERLINE_SHELL_CONTINUATION 1
 set POWERLINE_SHELL_SELECT 1
-set fish_function_path $fish_function_path "/usr/local/lib/python3.6/dist-packages/powerline/bindings/fish
-"
+set fish_function_path $fish_function_path "/usr/local/lib/python3.6/dist-packages/powerline/bindings/fish"
 powerline-setup
+set -gx PATH $PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /home/madamski/.local/bin
+set -gx PYTHONPATH $PYTHONPATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /home/madamski/.local/bin
